6ff7f564559c711aeb22d14f60659eea67976a4e
Hachi00008 <taiyo.uechi@gmail.com>
2026-05-11 21:25:17 +0900

    First Commit

---
+++ src/main/java/hachi00008/cursed_slash_swords/CursedSlashSwords.java
@@ -0,0 +1,15 @@
+package hachi00008.cursed_slash_swords;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class CursedSlashSwords implements ModInitializer {
+	public static final String MOD_ID = "cursed_slash_swords";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
---
+++ src/main/java/hachi00008/cursed_slash_swords/CursedSlashSwordsDataGenerator.java
@@ -0,0 +1,11 @@
+package hachi00008.cursed_slash_swords;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class CursedSlashSwordsDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
